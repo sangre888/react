3a7a3fc0aa9df055660a11bfe551e6d22af7cb76
Your Name <you@example.com>
2023-08-08 21:38:54 -0300

    myapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cartel from './components/cartel';
+function App() {
+  return (
+    <div className="App">
+      <Cartel />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/cartel.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './cartel.css'
+
+export default function Cartel() {
+  return (
+    <div className='cartelcontainer'>SANTIAGO</div>
+  )
+}
